24c80e47e0cb690a9a7524d7904022ea79827516
jeff <leterip@gmail.com>
2011-07-07 16:36:50 -0400

    Added HasCallback method and changed DelCallback to check if the string exists in the map before proceeding.

--- irc.go
+++ irc.go
@@ -167,13 +167,32 @@ func (conn *IRCConnection) AddCallback(cmd string, call Callback) {
 
 //Removes a callback from the handler
 func (conn *IRCConnection) DelCallback(cmd string, call Callback) {
-	calls := conn.callbacks[cmd]
+	calls, exists := conn.callbacks[cmd]
+	if !exists {
+		return
+	}
+
 	for idx, other := range calls {
 		if other == call {
 			conn.callbacks[cmd] = append(calls[:idx], calls[idx+1:]...)
 			break
 		}
 	}
+}
+
+func (conn *IRCConnection) HasCallback(cmd string, call Callback) (ret bool) {
+	calls, exists := conn.callbacks[cmd]
+	if !exists {
+		return false
+	}
+
+	for _, other := range calls {
+		if other == call {
+			return true
+		}
+	}
+
+	return false
 }
 
 //Convenience method for setting up join on connect
