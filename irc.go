b5d4f2cdbefd3e6d9d7f9226fb87055d10d1eb73
jeff <leterip@gmail.com>
2011-11-06 11:28:02 -0500

    added logging because im lazy

--- irc.go
+++ irc.go
@@ -216,9 +216,11 @@ func (conn *Connection) Handle() {
 			continue
 		}
 
+		fmt.Println(chunks)
 		if len(chunks) > 3 && len(chunks[3]) > 1 {
 			chunks[3] = chunks[3][1:] //strip off the left :
 		}
+		fmt.Println(chunks)
 
 		//Get the callbacks
 		callbacks, exists := conn.callbacks[strings.ToLower(chunks[1])]
